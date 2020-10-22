81c3a2a90ea5097caeb5c2af84f46abc5a119062
Caique <caiiquexd@gmail.com>
2020-10-22 18:29:24 -0300

    port

--- src/server.ts
+++ src/server.ts
@@ -17,29 +17,31 @@ const server = new ApolloServer({
   schema,
 });
 
+const PORT = process.env.PORT || 4000;
+
 createConnection().then(async () => {
-  server.listen().then(({ url }) => {
+  server.listen(PORT).then(({ url }) => {
     console.log(`Executando em ${url}`);
   });
 
-  const app = express();
-  app.use(express.json());
+  // const app = express();
+  // app.use(express.json());
 
-  app.get('/seed', async (req: Request, res: Response) => {
-    const profiles = await ProfilesListAction().save([{ label: 'Comum', name: 'Comum' }, { label: 'Admin', name: 'admin' }, { label: 'Master', name: 'master' }]);
-    const users = await UsersListAction().save([{ name: 'Joaozim Snow', password: '12345', email: 'jshsnow@email.com' }, { name: 'Jaiminho', password: '12345', email: 'jaiminho@gmail.com' }, { name: 'Larissa', password: '12345', email: 'lara@larissa.com' }]);
+  // app.get('/seed', async (req: Request, res: Response) => {
+  //   const profiles = await ProfilesListAction().save([{ label: 'Comum', name: 'Comum' }, { label: 'Admin', name: 'admin' }, { label: 'Master', name: 'master' }]);
+  //   const users = await UsersListAction().save([{ name: 'Joaozim Snow', password: '12345', email: 'jshsnow@email.com' }, { name: 'Jaiminho', password: '12345', email: 'jaiminho@gmail.com' }, { name: 'Larissa', password: '12345', email: 'lara@larissa.com' }]);
 
-    profiles[0].userprofile = [users[0], users[1]];
-    await ProfilesListAction().save(profiles[0]);
-    profiles[1].userprofile = [users[1], users[2]];
-    await ProfilesListAction().save(profiles[1]);
-    profiles[2].userprofile = [users[0], users[1], users[2]];
-    await ProfilesListAction().save(profiles[2]);
+  //   profiles[0].userprofile = [users[0], users[1]];
+  //   await ProfilesListAction().save(profiles[0]);
+  //   profiles[1].userprofile = [users[1], users[2]];
+  //   await ProfilesListAction().save(profiles[1]);
+  //   profiles[2].userprofile = [users[0], users[1], users[2]];
+  //   await ProfilesListAction().save(profiles[2]);
 
-    return res.json({ profiles, users });
-  });
+  //   return res.json({ profiles, users });
+  // });
 
-  app.listen(3333, () => {
-    console.log('Express 3333');
-  });
+  // app.listen(3333, () => {
+  //   console.log('Express 3333');
+  // });
 });
